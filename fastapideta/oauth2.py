1a2d869ade618273bf4f25632bd01de8f3aee300
AlexSitohov <lilinfinitemob@gmail.com>
2022-12-04 16:56:04 +0300

    upd 1.6 --deploy

--- fastapideta/oauth2.py
+++ fastapideta/oauth2.py
@@ -3,7 +3,9 @@ from fastapi.security import OAuth2PasswordBearer
 
 from fastapideta.JWT import verify_token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login", scheme_name="JWT")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login", scheme_name="JWT",
+                                     scopes={"me": "Read information about the current user.", "items": "Read items."},
+                                     )
 
 
 def get_current_user(data: str = Depends(oauth2_scheme)):
