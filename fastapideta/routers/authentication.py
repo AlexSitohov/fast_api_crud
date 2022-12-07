1986cfb1340dce53bcd1175e48207766e46939a7
AlexSitohov <lilinfinitemob@gmail.com>
2022-12-07 14:28:49 +0300

    upd 1.8 --deploy

--- fastapideta/routers/authentication.py
+++ fastapideta/routers/authentication.py
@@ -8,13 +8,13 @@ from ..JWT import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from ..schemas import Login, User, ChangePassword
 from ..database import get_db
 from .. import models, oauth2
-from ..hashing import verify_password,bcrypt
+from ..hashing import verify_password, bcrypt
 
 router = APIRouter(tags=['authentication'])
 
 
-@router.post('/login/')
-async def login(data:  Login, db: Session = Depends(get_db)):
+@router.post('/login')
+async def login(data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.name == data.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid username')
@@ -25,5 +25,3 @@ async def login(data:  Login, db: Session = Depends(get_db)):
         data={"sub": user.name}
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
-
